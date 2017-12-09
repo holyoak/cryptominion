b5c281af72675dc8075aacf13e1da323a9681cb4
holyoak <mericsmith@yahoo.com>
2017-12-08 22:55:06 -0800

    svg practice

--- src/client/components/splash/Splash.vue
+++ src/client/components/splash/Splash.vue
@@ -17,8 +17,7 @@
     :label="activeLabel"
     :userInput="activeInput">
   </component>
-  <div>
-    <img src="./drawing.svg">
+  <div id="test">
   </div>
 </div>
 </template>
@@ -26,6 +25,7 @@
 <script>
 import TextInput from './TextInput'
 import OtherTextInput from './TextInput2'
+import Practice from './drawing.svg.js'
 
 export default {
   name: 'splash-view',
@@ -38,6 +38,7 @@ export default {
   data () {
     return {
       active: false,
+      practice: Practice,
       currentView: 'text-input',
       label: 'Input Component Label',
       userInput: 'placeholder',
@@ -56,6 +57,10 @@ export default {
     activeInput () {
       return this.active === true ? this.userInput : this.otherUserInput
     }
+  },
+
+  mounted () {
+    document.getElementById('test').innerHTML = Practice
   },
 
   methods: {
